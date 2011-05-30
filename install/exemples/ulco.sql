8ebf658e59bb0ba4d4f0b494213407ca80dd2a89
Eric Ramat <ramat@lil.univ-littoral.fr>
2011-05-30 15:52:59 +0200

    Ajout d'enregistrements

--- install/exemples/ulco.sql
+++ install/exemples/ulco.sql
@@ -1,34 +1,12 @@
-INSERT INTO `edt_departement` VALUES (1, 'Informatique');
-INSERT INTO `edt_departement` VALUES (2, 'Langues');
-INSERT INTO `edt_departement` VALUES (3, 'Math&eacute;matiques');
-INSERT INTO `edt_departement` VALUES (4, 'Physique');
-INSERT INTO `edt_departement` VALUES (5, 'Droit');
-INSERT INTO `edt_departement` VALUES (6, 'Eco-gestion');
-INSERT INTO `edt_departement` VALUES (7, 'EEA');
-INSERT INTO `edt_departement` VALUES (8, 'Biologie');
-INSERT INTO `edt_departement` VALUES (9, 'STAPS');
-INSERT INTO `edt_departement` VALUES (10, 'G&eacute;ographie');
-INSERT INTO `edt_departement` VALUES (11, 'Histoire');
-
 INSERT INTO `edt_domaine` VALUES (1, 'Sciences et Technologies');
 INSERT INTO `edt_domaine` VALUES (2, 'Droit');
 INSERT INTO `edt_domaine` VALUES (3, 'Lettres et langues');
 INSERT INTO `edt_domaine` VALUES (4, 'Sciences Humaines et Sociales');
 INSERT INTO `edt_domaine` VALUES (5, 'Sciences &eacute;co et gestion');
 
-INSERT INTO `edt_grade` VALUES (1, 'ATER', 192);
-INSERT INTO `edt_grade` VALUES (2, 'Etudiant', 96);
-INSERT INTO `edt_grade` VALUES (3, 'IATOS', 20);
-INSERT INTO `edt_grade` VALUES (4, 'Ma&icirc;tre de conf&eacute;rence', 192);
-INSERT INTO `edt_grade` VALUES (5, 'Ma&icirc;tre de conf&eacute;rence \r\nhors\r\nclasse', 192);
-INSERT INTO `edt_grade` VALUES (6, 'Moniteur CIES', 64);
-INSERT INTO `edt_grade` VALUES (7, 'PRAG', 384);
-INSERT INTO `edt_grade` VALUES (8, 'Professeur 1&egrave;re classe', 192);
-INSERT INTO `edt_grade` VALUES (9, 'Professeur 2&egrave;me classe', 192);
-INSERT INTO `edt_grade` VALUES (10, 'Professeur hors classe', 192);
-INSERT INTO `edt_grade` VALUES (11, 'Vacataire', 160);
-INSERT INTO `edt_grade` VALUES (12, '1/2 ATER', 96);
-INSERT INTO `edt_grade` VALUES (13, 'PRCE', 384);
+INSERT INTO `edt_niveau` VALUES (1, 'Licence', 3);
+INSERT INTO `edt_niveau` VALUES (3, 'Doctorat', 3);
+INSERT INTO `edt_niveau` VALUES (2, 'Master', 2);
 
 INSERT INTO `edt_mention` VALUES (1, 'Informatique');
 INSERT INTO `edt_mention` VALUES (2, 'Math&eacute;matiques');
@@ -52,7 +30,43 @@ INSERT INTO `edt_mention` VALUES (20, 'Culture et m&eacute;dias');
 INSERT INTO `edt_mention` VALUES (21, 'G&eacute;ographie');
 INSERT INTO `edt_mention` VALUES (22, 'Histoire');
 
-INSERT INTO `edt_pole` VALUES (1, 'Calais');
-INSERT INTO `edt_pole` VALUES (2, 'Boulogne sur mer');
-INSERT INTO `edt_pole` VALUES (3, 'Dunkerque');
-INSERT INTO `edt_pole` VALUES (4, 'Saint Omer');
+INSERT INTO `edt_specialite` VALUES (1, 'ISIDIS');
+INSERT INTO `edt_specialite` VALUES (2, 'I2L');
+INSERT INTO `edt_specialite` VALUES (3, 'INS3i');
+INSERT INTO `edt_specialite` VALUES (4, 'EIP');
+
+INSERT INTO `edt_pole` VALUES (1, 'CGU de Calais');
+
+INSERT INTO `edt_departement` VALUES (1, 'Informatique');
+INSERT INTO `edt_departement` VALUES (2, 'Langues');
+INSERT INTO `edt_departement` VALUES (3, 'Math&eacute;matiques');
+INSERT INTO `edt_departement` VALUES (4, 'Physique');
+INSERT INTO `edt_departement` VALUES (5, 'Droit');
+INSERT INTO `edt_departement` VALUES (6, 'Eco-gestion');
+INSERT INTO `edt_departement` VALUES (7, 'EEA');
+INSERT INTO `edt_departement` VALUES (8, 'Biologie');
+INSERT INTO `edt_departement` VALUES (9, 'STAPS');
+INSERT INTO `edt_departement` VALUES (10, 'G&eacute;ographie');
+INSERT INTO `edt_departement` VALUES (11, 'Histoire');
+
+INSERT INTO `edt_grade` VALUES (1, 'ATER', 192);
+INSERT INTO `edt_grade` VALUES (2, 'Etudiant', 96);
+INSERT INTO `edt_grade` VALUES (3, 'IATOS', 20);
+INSERT INTO `edt_grade` VALUES (4, 'Ma&icirc;tre de conf&eacute;rence', 192);
+INSERT INTO `edt_grade` VALUES (5, 'Ma&icirc;tre de conf&eacute;rence hors classe', 192);
+INSERT INTO `edt_grade` VALUES (6, 'Moniteur CIES', 64);
+INSERT INTO `edt_grade` VALUES (7, 'PRAG', 384);
+INSERT INTO `edt_grade` VALUES (8, 'Professeur 1&egrave;re classe', 192);
+INSERT INTO `edt_grade` VALUES (9, 'Professeur 2&egrave;me classe', 192);
+INSERT INTO `edt_grade` VALUES (10, 'Professeur hors classe', 192);
+INSERT INTO `edt_grade` VALUES (11, 'Vacataire', 160);
+INSERT INTO `edt_grade` VALUES (12, '1/2 ATER', 96);
+INSERT INTO `edt_grade` VALUES (13, 'PRCE', 384);
+
+INSERT INTO `edt_type_seance` VALUES (1, 'CM');
+INSERT INTO `edt_type_seance` VALUES (2, 'TD');
+INSERT INTO `edt_type_seance` VALUES (3, 'TP');
+INSERT INTO `edt_type_seance` VALUES (4, 'Examen');
+
+INSERT INTO `edt_type_salle` VALUES (1, 'Amphi');
+INSERT INTO `edt_type_salle` VALUES (2, 'TD');
