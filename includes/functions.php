39cf3b5a72ebf001d8404e4e441c7bb7d9f03582
Eric Ramat <ramat@lisic.univ-littoral.fr>
2011-05-31 10:33:23 +0200

    Correction de bugs dans la gestion des superviseurs

--- includes/functions.php
+++ includes/functions.php
@@ -2,82 +2,85 @@
 
 // fonction qui affiche les messages d'erreur
 function erreur($msg) {
-    echo "<table align='center'><tr><td class='erreur'>Erreur !<br>",$msg,"</td></tr></table>\n";
+  echo "<table align='center'><tr><td class='erreur'>Erreur !<br>",$msg,
+    "</td></tr></table>\n";
 }
 
 function est_administrateur($user_rank) {
-    return ($user_rank == 1);
+  return ($user_rank == 1);
 }
 function est_enseignant($user_rank) {
-    return ($user_rank == 2);
+  return ($user_rank == 2);
 }
 function est_etudiant($user_rank) {
-    return ($user_rank == 3);
+  return ($user_rank == 3);
 }
 function est_directeur_etude($user_rank) {
-    return ($user_rank == 4);
+  return ($user_rank == 4);
 }
 function est_president_du_jury($user_rank) {
-    return ($user_rank == 5);
+  return ($user_rank == 5);
 }
 function est_directeur_de_departement($user_rank) {
-    return ($user_rank == 6);
+  return ($user_rank == 6);
 }
 function est_secretaire($user_rank) {
-    return ($user_rank == 7);
+  return ($user_rank == 7);
 }
 function est_superviseur($user_rank) {
-    return ($user_rank == 8);
+  return ($user_rank == 8);
 }
-
 
 // Affiche une liste déroulante avec les niveaux existants
 function choix_niveau($id_niveau, $val_retour) {
-    global $prefix_tables, $DB;
-    $res = $DB->Execute("SELECT id, libelle
-                        FROM ".$prefix_tables."niveau");
-    echo "<td>Niveau :</td>\n";
-    echo "<td>\n";
-    if ($res->RecordCount()) {
-        echo "<select name=\"niveau\" onChange=\"choix.value=",$val_retour,"; submit();\">\n";
-        echo "<option value=\"0\"></option>\n";
-        while ($row = $res->FetchRow()) {
-            echo "<option value=\"",$row[0],"\"";
-            if ($id_niveau==$row[0]) echo " selected";
-            echo ">",$row[1],"</option>\n";
-        }
-        echo "</select>\n";
-    } else {
-        echo "<i>Aucun niveau enregistr&eacute;</i>\n";
+  global $prefix_tables, $DB;
+
+  $res = $DB->Execute("SELECT id, libelle
+                       FROM ".$prefix_tables."niveau");
+  echo "<td>Niveau :</td>\n";
+  echo "<td>\n";
+  if ($res->RecordCount()) {
+    echo "<select name=\"niveau\" onChange=\"choix.value=",$val_retour,"; submit();\">\n";
+    echo "<option value=\"0\"></option>\n";
+    while ($row = $res->FetchRow()) {
+      echo "<option value=\"",$row[0],"\"";
+      if ($id_niveau==$row[0]) echo " selected";
+      echo ">",$row[1],"</option>\n";
     }
-    echo "</td>\n";
-    $res->Close();
+    echo "</select>\n";
+  } else {
+    echo "<i>Aucun niveau enregistr&eacute;</i>\n";
+  }
+  echo "</td>\n";
+  $res->Close();
 }
 
 // Affiche la liste des années en fonction du niveau
 function choix_annee($id_niveau, $id_annee, $val_retour) {
-    global $prefix_tables, $DB;
-    $res = $DB->Execute("SELECT DISTINCT(annee)
-                        FROM ".$prefix_tables."diplome
-                        WHERE id_niveau=?
-                        ORDER BY annee asc",
-                        array($id_niveau));
-    echo "<td>Ann&eacute;e :</td>\n";
-    echo "<td>\n";
-    if ($res->RecordCount()) {
-        echo "<select name=\"annee\" onChange=\"choix.value=",$val_retour,"; submit();\">\n";
-        echo "<option value=\"0\"></option>\n";
-        while ($row = $res->FetchRow()) {
-            echo "<option value=\"",$row[0],"\"";
-            if ($id_annee==$row[0]) echo " selected";
-            echo ">",$row[0],"</option>\n";
-        }
-        echo "</select>\n";
-    } else {
-        echo "<i>Aucun dipl&ocirc;me enregistr&eacute;</i>\n";
+  global $prefix_tables, $DB;
+
+  $res = $DB->Execute("SELECT DISTINCT(annee)
+                       FROM ".$prefix_tables."diplome
+                       WHERE id_niveau=?
+                       ORDER BY annee asc",
+		      array($id_niveau));
+  echo "<td>Ann&eacute;e :</td>\n";
+  echo "<td>\n";
+  if ($res->RecordCount()) {
+    echo "<select name=\"annee\" onChange=\"choix.value=",$val_retour,
+      "; submit();\">\n";
+    echo "<option value=\"0\"></option>\n";
+    while ($row = $res->FetchRow()) {
+      echo "<option value=\"",$row[0],"\"";
+      if ($id_annee==$row[0]) echo " selected";
+      echo ">",$row[0],"</option>\n";
     }
-    echo "</td>\n";
-    $res->Close();
+    echo "</select>\n";
+  } else {
+    echo "<i>Aucun dipl&ocirc;me enregistr&eacute;</i>\n";
+  }
+  echo "</td>\n";
+  $res->Close();
 }
 
 // Affiche une liste déroulante avec les domaines existants
@@ -133,7 +136,7 @@ function choix_diplome($id_niveau, $annee, $id_domaine, $id_diplome, $val_retour
 function choix_option($id_diplome, $id_option, $val_retour, $est_obligatoire) {
     global $prefix_tables, $DB;
     $req = "SELECT id, nom
-            FROM ".$prefix_tables."option 
+            FROM ".$prefix_tables."option
             WHERE id_diplome=?
             ORDER BY nom ASC";
     $res = $DB->Execute($req, array($id_diplome));
@@ -163,18 +166,18 @@ function choix_groupe($id_diplome, $id_option, $id_groupe, $val_retour, $est_obl
     if ($id_option > 0) {
         $req = "SELECT id, nom
                 FROM ".$prefix_tables."groupe g
-                WHERE id_option=? 
+                WHERE id_option=?
                 ORDER BY nom ASC";
         $req_array = array($id_option);
     } else {
         $req = "SELECT id, nom
                 FROM ".$prefix_tables."groupe g
-                WHERE id_diplome=? 
+                WHERE id_diplome=?
                 ORDER BY nom ASC";
         $req_array = array($id_diplome);
     }
     $res = $DB->Execute($req, $req_array);
-    
+
     if ($res->RecordCount()) {
     echo "<td>Groupe";
     if (!$est_obligatoire) echo " (facultatif)";
@@ -187,7 +190,7 @@ function choix_groupe($id_diplome, $id_option, $id_groupe, $val_retour, $est_obl
                   (($id_groupe==$row[0]) ? " selected" : ""),
                   ">",$row[1];
             // Types associés au groupe
-            $req2 = "SELECT libelle 
+            $req2 = "SELECT libelle
                     FROM ".$prefix_tables."groupe_type gt, ".$prefix_tables."type_sceance ts
                     WHERE gt.id_groupe=? AND ts.id=gt.id_type
                     ORDER BY libelle ASC";
@@ -258,7 +261,7 @@ function choix_diplome_dde(&$id_diplome, $val_retour) {
         echo "<option value=\"0\"></option>\n";
         while ($row = $res->FetchRow()) {
             echo "<option value=\"",$row[0],"\"",
-                  (($id_diplome==$row[0]) ?" selected" : ""), 
+                  (($id_diplome==$row[0]) ?" selected" : ""),
                   ">",$row[1],"</option>\n";
         }
         echo "</select>\n";
@@ -304,7 +307,7 @@ function choix_diplome_ddd($id_diplome, $val_retour) {
 function choix_diplome_sec($id_diplome, $val_retour) {
     global $prefix_tables, $DB;
     $req = "SELECT d.id_diplome, d.sigle_complet
-            FROM ".$prefix_tables."diplome d, ".$prefix_tables."secretaire s, 
+            FROM ".$prefix_tables."diplome d, ".$prefix_tables."secretaire s,
                  ".$prefix_tables."secretaire_occupe_diplome sod
             WHERE d.id_diplome=sod.id_diplome AND sod.id_secretaire=s.id_secretaire AND s.id_secretaire=?";
     $res = $DB->Execute($req, array($_SESSION["id"]));
--- superviseur/superviseur.php
+++ superviseur/superviseur.php
@@ -1,50 +1,61 @@
 <?php
-  // Pour bloquer l'accès direct à cette page
+
+// Pour bloquer l'accès direct à cette page
 if (!defined("acces_ok"))
   exit;
-    
+
 // Affichage de l'entête
 entete("Gestion des superviseurs");
 
 // Afficher la liste des secrétaires
 function afficher_liste() {
-    global $prefix_tables, $DB;
-    echo "<p align=\"center\"><b>Liste des superviseurs :</b></p>\n";
-    $res = $DB->Execute("select id, concat(nom,' ',prenom) as nom_complet, id_enseignant
-                        FROM ".$prefix_tables."superviseur 
-                        order by nom_complet asc");
-    if ($res->RecordCount()) {
-        echo "<p align=\"center\">\n";
-        while ($row = $res->FetchRow()) {
-            if ($row[2] == 0) {
-                echo "<a href=\"index.php?page=superviseur&id=",$row[0],"\">", $row[1], "</a><br />\n";
-            } else {
-                $row2 = $DB->GetOne("select concat(nom,' ',prenom) as nom_complet 
-                                      from ".$prefix_tables."enseignant
-                                      where id_enseignant = ?
-                                      order by nom_complet asc",
-                                      array($row[2]));
-                echo "<a href=\"index.php?page=superviseur&id=",$row[0],"\">", $row2, "</a><br />\n";
-            }
-        }
-        echo "</p>\n";
-    } else {
-        echo "<p align=\"center\"><i>Aucun superviseur enregistr&eacute;</p>\n";
+  global $prefix_tables, $DB;
+
+  echo "<p align=\"center\"><b>Liste des superviseurs :</b></p>\n";
+  $req = "SELECT id, (nom || ' ' || prenom) as nom_complet, id_enseignant
+          FROM ".$prefix_tables."superviseur
+          ORDER BY nom_complet asc";
+  $res = $DB->Execute($req);
+  if ($res->RecordCount()) {
+    echo "<p align=\"center\">\n";
+    while ($row = $res->FetchRow()) {
+      if ($row[2] == 0) {
+	echo "<a href=\"index.php?page=superviseur&id=",$row[0],"\">",
+	  $row[1], "</a><br />\n";
+      } else {
+	$row2 = $DB->GetOne("SELECT (nom || ' ' || prenom) as nom_complet
+                             FROM ".$prefix_tables."enseignant
+                             WHERE id_enseignant = ?
+                             ORDER BY nom_complet asc",
+			    array($row[2]));
+	echo "<a href=\"index.php?page=superviseur&id=",$row[0],"\">",
+	  $row2, "</a><br />\n";
+      }
     }
-    echo "<form method=\"post\" action=\"index.php?page=superviseur\"><p align=\"center\"><input type=\"submit\" name=\"nouveau\" value=\"Nouveau superviseur\" /></p></form>\n";
+    echo "</p>\n";
+  } else {
+    echo "<p align=\"center\"><i>Aucun superviseur enregistr&eacute;</p>\n";
+  }
+  echo "<form method=\"post\" action=\"index.php?page=superviseur\"><p align=\"center\"><input type=\"submit\" name=\"nouveau\" value=\"Nouveau superviseur\" /></p></form>\n";
 }
 
 function affiche_details_type($type, $selected) {
-    global $prefix_tables, $DB;
-    $res = $DB->Execute("SELECT id, libelle
-                         FROM ".$prefix_tables.$type."
-                         ORDER  BY libelle ASC");
-    while ($row = $res->FetchRow()) {
-        echo "<option value=\"",$row[0],"\"";
-        if ($row[0] == $selected) echo " selected";
-        echo ">",$row[1],"</option>\n";
-    }
-    $res->Close();
+  global $prefix_tables, $DB;
+
+  $req = "SELECT id_enseignant, (nom || ' ' || prenom)
+                       FROM ".$prefix_tables.$type."
+                       ORDER BY nom ASC";
+
+  echo "<option value=\"0\"";
+  if ($selected == 0) echo " selected";
+  echo "></option>\n";
+  $res = $DB->Execute($req);
+  while ($row = $res->FetchRow()) {
+    echo "<option value=\"",$row[0],"\"";
+    if ($row[0] == $selected) echo " selected";
+    echo ">",$row[1],"</option>\n";
+  }
+  $res->Close();
 }
 
 function charger_donnees_formulaire($id_superviseur) {
@@ -64,13 +75,14 @@ function afficher_formulaire($id_superviseur) {
   } elseif ($id_superviseur) {
     $donnees = charger_donnees_formulaire($id_superviseur);
   } else {
-    $donnees = array("nom" => "", "prenom" => "", "tel" => "", "fax" => "", "email" => "", "enseignant" => 0);
+    $donnees = array("nom" => "", "prenom" => "", "tel" => "", "fax" => "",
+		     "email" => "", "enseignant" => 0);
   }
   echo "<form method=\"post\" action=\"index.php?page=superviseur\">\n";
   echo "<table align=\"center\">\n";
   echo "<input type=\"hidden\" name=\"id_superviseur\" value=\"",$id_superviseur,"\">\n";
-  echo "<tr><td>Nom</td><td><input type=\"text\" name=\"nom\" size=\"20\" maxlength=\"32\" value=\"",$donnees["nom"],"\"></td></tr>\n";
-  echo "<tr><td>Pr&eacute;nom</td><td><input type=\"text\" name=\"prenom\" size=\"20\" maxlength=\"32\" value=\"",$donnees["prenom"],"\"></td></tr>\n";
+  echo "<tr><td>Nom</td><td><input type=\"text\" name=\"nom\" size=\"20\" maxlength=\"64\" value=\"",$donnees["nom"],"\"></td></tr>\n";
+  echo "<tr><td>Pr&eacute;nom</td><td><input type=\"text\" name=\"prenom\" size=\"20\" maxlength=\"64\" value=\"",$donnees["prenom"],"\"></td></tr>\n";
   echo "<tr><td>T&eacute;l&eacute;phone</td><td><input type=\"text\" name=\"tel\" size=\"20\" maxlength=\"16\" value=\"",$donnees["tel"],"\"></td></tr>\n";
   echo "<tr><td>Fax</td><td><input type=\"text\" name=\"fax\" size=\"20\" maxlength=\"16\" value=\"",$donnees["fax"],"\"></td></tr>\n";
   echo "<tr><td>Email</td><td><input type=\"text\" name=\"email\" size=\"30\" maxlength=\"50\" value=\"",$donnees["email"],"\"></td></tr>\n";
@@ -91,55 +103,67 @@ function afficher_formulaire($id_superviseur) {
 
 // Création du pseudo du superviseur
 function creer_pseudo($nom, $prenom, $id) {
-    global $prefix_tables, $DB;
-    $id_type = 8; // id du type superviseur
-    $pseudo = substr(strtolower($prenom), 0, 1).strtolower($nom);
-    
-    // On teste d'abord si le pseudo existe déjà
-    $exist = $DB->GetOne("SELECT COUNT(*) FROM ".$prefix_tables."users WHERE login='".$pseudo."'");
-    if ($exist) {
-        $i = 0;
-        do {
-            $i++;
-            $exist = $DB->GetOne("SELECT COUNT(*) FROM ".$prefix_tables."users WHERE login='".$pseudo.$i."'");
-        } while ($exist);
-        $pseudo .= $i;
-    }
-    
-    // Ajout des données
-    $DB->Execute("INSERT INTO ".$prefix_tables."users (login, password, actif) VALUES (?, ?, 0)",
-                  array($pseudo, md5("")));
-    $id_user = $DB->Insert_ID();
-    $DB->Execute("INSERT INTO ".$prefix_tables."user_est_de_type (id_user, id_type, id) VALUES (?, ?, ?)",
-                  array($id_user, $id_type, $id));
+  global $prefix_tables, $DB;
+
+  $id_type = 8; // id du type superviseur
+  $pseudo = substr(strtolower($prenom), 0, 1).strtolower($nom);
+
+  // On teste d'abord si le pseudo existe déjà
+  $exist = $DB->GetOne("SELECT COUNT(*) FROM ".$prefix_tables.
+		       "user WHERE login='".$pseudo."'");
+  if ($exist) {
+    $i = 0;
+    do {
+      $i++;
+      $exist = $DB->GetOne("SELECT COUNT(*) FROM ".$prefix_tables.
+			   "user WHERE login='".$pseudo.$i."'");
+    } while ($exist);
+    $pseudo .= $i;
+  }
+
+  // Ajout des données
+  $id_user = $DB->GenID($prefix_tables."user_id_seq");
+  $DB->Execute("INSERT INTO ".$prefix_tables.
+	       "user (id_user,login, password, actif) VALUES (?, ?, ?, FALSE)",
+	       array($id_user,$pseudo, md5("")));
+
+  $req = "INSERT INTO ".$prefix_tables.
+    "user_est_de_type (id_user, id_type, id) VALUES (".
+    $id_user.",".$id_type.", ".$id.")";
+  $DB->Execute($req);
 }
 
 // Ajouter un nouveau superviseur
 function ajouter_superviseur($donnees) {
-    global $prefix_tables, $DB;
-    $DB->Execute("insert into ".$prefix_tables."superviseur
-                 (nom, prenom, tel, fax, email, id_enseignant)
-                  values (?, ?, ?, ?, ?, ?)",
-                 array($donnees["nom"], $donnees["prenom"], $donnees["tel"], $donnees["fax"], $donnees["email"], $donnees["enseignant"]));
-    $id = $DB->Insert_Id();
-    if ($donnees["enseignant"] == 0)  {
-        creer_pseudo($donnees["nom"], $donnees["prenom"], $id);
-    } else {
-        // il faut ajouter ce type à l'enseignant
-        // On récupère l'identifiant de connexion de l'enseignant 
-        // de type $nom_type
-        $id_user = $DB->GetOne("select id_user
-                                from ".$prefix_tables."user_est_de_type 
-                                where id_type=2 and id=?",
-                                array($donnees["enseignant"]));
-        // Ajout des droits si l'enseignant en question ne les a pas déjà
-        if (!$id_user) {
-            $DB->Execute("insert into ".$prefix_tables."user_est_de_type 
-                         (id_user, id_type, id) values (?, ?, ?)",
-                         array($id_user, 8 , $donnees["enseignant"]));
-        }
+  global $prefix_tables, $DB;
+
+  $id = $DB->GenID($prefix_tables."superviseur_id_seq");
+  $req = "INSERT INTO ".$prefix_tables."superviseur
+                 (id, nom, prenom, tel, fax, email, id_enseignant)
+                  VALUES (".$id.", '".$donnees["nom"]."', '".
+    $donnees["prenom"]."', '".$donnees["tel"]."', '".$donnees["fax"]."', '".
+    $donnees["email"]."',".$donnees["enseignant"].")";
+
+  $DB->Execute($req);
+  if ($donnees["enseignant"] == 0)  {
+    creer_pseudo($donnees["nom"], $donnees["prenom"], $id);
+  } else {
+    // il faut ajouter ce type à l'enseignant
+    // On récupère l'identifiant de connexion de l'enseignant
+    // de type $nom_type
+    $req = "SELECT id_user
+            FROM ".$prefix_tables."user_est_de_type
+            WHERE id_type=2 and id=".$donnees["enseignant"];
+    $id_user = $DB->GetOne($req);
+    // Ajout des droits si l'enseignant en question ne les a pas déjà
+    if ($id_user != 0) {
+      $req = "INSERT INTO ".$prefix_tables."user_est_de_type
+              (id_user, id_type, id) VALUES (".$id_user.", 8, ".
+	$donnees["enseignant"].")";
+      $DB->Execute($req);
     }
-    return $id;
+  }
+  return $id;
 }
 
 // Mise à jour des informations sur la secrétaire
@@ -149,7 +173,8 @@ function mise_a_jour_superviseur($donnees) {
                   set nom=?, prenom=?, tel=?, fax=?, email=?, id_enseignant=?
                   where id=?",
                   array($donnees["nom"], $donnees["prenom"], $donnees["tel"],
-                        $donnees["fax"], $donnees["email"], $donnees["enseignant"], $donnes["id_superviseur"]));
+                        $donnees["fax"], $donnees["email"],
+			$donnees["enseignant"], $donnees["id_superviseur"]));
     return $donnees["id_superviseur"];
 }
 
@@ -160,7 +185,7 @@ function verifier_formulaire() {
   $_POST["tel"] = trim($_POST["tel"]);
   $_POST["fax"] = trim($_POST["fax"]);
   $_POST["email"] = trim($_POST["email"]);
-    
+
   $msg_erreur = "";
   if ($_POST["enseignant"] == 0) {
     if (empty($_POST["nom"])) $msg_erreur .= "Le champ <i>Nom</i> doit &ecirc;tre rempli.<br>";
@@ -190,11 +215,12 @@ function verifier_formulaire() {
 
 // Vérifier si l'id du superviseur existe
 function verifie_existence_superviseur($id_superviseur) {
-    global $prefix_tables, $DB;
-    return $DB->GetOne("select count(*)
-                        from ".$prefix_tables."superviseur
-                        where id=?",
-                        array($id_superviseur));
+  global $prefix_tables, $DB;
+
+  return $DB->GetOne("select count(*)
+                      from ".$prefix_tables."superviseur
+                      where id=?",
+		     array($id_superviseur));
 }
 
 // Suppression d'un superviseur
@@ -216,18 +242,18 @@ if (isset($_GET["id"])) {
     erreur("Param&egrave;tres incorrects");
     afficher_liste();
   }
- } elseif (isset($_POST["supprimer"]) && $_POST["supprimer"]==1) { // Suppression d'un superviseur
-   supprimer_superviseur($_POST["id_superviseur"]);
-   echo "<p align=\"center\"><b>Superviseur supprim&eacute;</b></p>\n";
-   afficher_liste();
-   } elseif (isset($_POST["nom"])) { // Vériication des données saisies
-     verifier_formulaire();
-     } elseif (isset($_POST["nouveau"])) { // Formulaire pour une nouvelle secrétaire
-       echo "<p align=\"center\"><b>Nouveau superviseur :</b></p>\n";
-       afficher_formulaire(0);
-       echo "<p align=\"center\"><a href=\"index.php?page=superviseur\">Retour &agrave; la liste des superviseurs</a></p>\n";
-       } else {
+} elseif (isset($_POST["supprimer"]) && $_POST["supprimer"]==1) { // Suppression d'un superviseur
+  supprimer_superviseur($_POST["id_superviseur"]);
+  echo "<p align=\"center\"><b>Superviseur supprim&eacute;</b></p>\n";
   afficher_liste();
- }
+} elseif (isset($_POST["nom"])) { // Vérification des données saisies
+  verifier_formulaire();
+} elseif (isset($_POST["nouveau"])) { // Formulaire pour un nouveau superviseur
+  echo "<p align=\"center\"><b>Nouveau superviseur :</b></p>\n";
+  afficher_formulaire(0);
+  echo "<p align=\"center\"><a href=\"index.php?page=superviseur\">Retour &agrave; la liste des superviseurs</a></p>\n";
+} else {
+  afficher_liste();
+}
 
 ?>
