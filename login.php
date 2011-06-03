77d1bbea3381053366cecb500933deb1ebcd5675
Eric Ramat <ramat@lil.univ-littoral.fr>
2011-06-03 16:22:28 +0200

    Ajout de la licence au fichier login.php

--- login.php
+++ login.php
@@ -1,4 +1,30 @@
 <?php
+/*
+ * @file login.php
+ *
+ * This file is part of ScolariteNet web application
+ * https://gitorious.org/scolaritenet
+ *
+ * Copyright (c) 2011 ULCO http://www.univ-littoral.fr
+ *
+ * See the AUTHORS or Authors.txt file for copyright owners and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+?>
+
+<?php
 
 // Pour bloquer l'accès direct à cette page
 if (!defined("acces_ok"))
